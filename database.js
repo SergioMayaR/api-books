eaaefca5e90e6344cd12ef47736c72be88893f34
unknown <saxel.12194@gmail.com>
2022-07-29 14:45:16 -0500

    V2

--- database.js
+++ database.js
@@ -1,11 +1,16 @@
 const mysql = require("mysql"); //Se declara mysql
 //Se genera la conexión a mysql
 const mysqlConacttion = mysql.createConnection({
-  host: "34.67.244.163", //Se declara el host de la bd
+  host: "162.240.215.202", //Se declara el host de la bd
+  user: "jeerel_userRoot", //Obtenemos el usuario al que se conectara
+  password: "XML7E&)f@zx)", //Obtiene el password
+  database: "jeerel_librodetodomexico" //Se hace la referencia al bd
+  /* host: "34.67.244.163", //Se declara el host de la bd
   user: "ptree", //Obtenemos el usuario al que se conectara
   password: "Ptree2021ñ", //Obtiene el password
-  database: "Alpha", //Se hace la referencia al bd
+  database: "Alpha", //Se hace la referencia al bd */
 });
+
 //Valida que la conexión este funcionando
 mysqlConacttion.connect(function (error) {
   if (error) {
