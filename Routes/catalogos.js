6aa92d7f5f32931c020781948cfc47b1ba93afdf
unknown <saxel.12194@gmail.com>
2022-12-23 13:53:53 -0600

    V5

--- Routes/catalogos.js
+++ Routes/catalogos.js
@@ -33,8 +33,10 @@ const {
 const imageBase64Data = "iVBORw0KGgoAAAANSUhEUgAAAIAAAACACAMAAAD04JH5AAACzVBMVEUAAAAAAAAAAAAAAAA/AD8zMzMqKiokJCQfHx8cHBwZGRkuFxcqFSonJyckJCQiIiIfHx8eHh4cHBwoGhomGSYkJCQhISEfHx8eHh4nHR0lHBwkGyQjIyMiIiIgICAfHx8mHh4lHh4kHR0jHCMiGyIhISEgICAfHx8lHx8kHh4jHR0hHCEhISEgICAlHx8kHx8jHh4jHh4iHSIhHCEhISElICAkHx8jHx8jHh4iHh4iHSIhHSElICAkICAjHx8jHx8iHh4iHh4hHiEhHSEkICAjHx8iHx8iHx8hHh4hHiEkHSEjHSAjHx8iHx8iHx8hHh4kHiEkHiEjHSAiHx8hHx8hHh4kHiEjHiAjHSAiHx8iHx8hHx8kHh4jHiEjHiAjHiAiICAiHx8kHx8jHh4jHiEjHiAiHiAiHSAiHx8jHx8jHx8jHiAiHiAiHiAiHSAiHx8jHx8jHx8iHiAiHiAiHiAjHx8jHx8jHx8jHx8iHiAiHiAiHiAjHx8jHx8jHx8iHx8iHSAiHiAjHiAjHx8jHx8hHx8iHx8iHyAiHiAjHiAjHiAjHh4hHx8iHx8iHx8iHyAjHSAjHiAjHiAjHh4hHx8iHx8iHx8jHyAjHiAhHh4iHx8iHx8jHyAjHSAjHSAhHiAhHh4iHx8iHx8jHx8jHyAjHSAjHSAiHh4iHh4jHx8jHx8jHyAjHyAhHSAhHSAiHh4iHh4jHx8jHx8jHyAhHyAhHSAiHSAiHh4jHh4jHx8jHx8jHyAhHyAhHSAiHSAjHR4jHh4jHx8jHx8hHyAhHyAiHSAjHSAjHR4jHh4jHx8hHx8hHyAhHyAiHyAjHSAjHR4jHR4hHh4hHx8hHyAiHyAjHyAjHSAjHR4jHR4hHh4hHx8hHyAjHyAjHyAjHSAjHR4hHR4hHR4hHx8iHyAjHyAjHyAjHSAhHR4hHR4hHR4hHx8jHyAjHyAjHyAjHyC9S2xeAAAA7nRSTlMAAQIDBAUGBwgJCgsMDQ4PEBESExQVFxgZGhscHR4fICEiIyQlJicoKSorLS4vMDEyMzQ1Njc4OTo7PD0+P0BBQkNERUZISUpLTE1OUFFSU1RVVllaW1xdXmBhYmNkZWZnaGprbG1ub3Byc3R1dnd4eXp8fn+AgYKDhIWGiImKi4yNj5CRkpOUlZaXmJmam5ydnp+goaKjpKaoqqusra6vsLGys7S1tri5uru8vb6/wMHCw8TFxsfIycrLzM3Oz9DR0tPU1dbX2Nna29zd3t/g4eLj5OXm5+jp6uvs7e7v8PHy8/T19vf4+fr7/P3+fkZpVQAABcBJREFUGBntwftjlQMcBvDnnLNL22qzJjWlKLHFVogyty3SiFq6EZliqZGyhnSxsLlMRahYoZKRFcul5dKFCatYqWZaNKvWtrPz/A2+7/b27qRzec/lPfvl/XxgMplMJpPJZDKZAtA9HJ3ppnIez0KnSdtC0RCNznHdJrbrh85wdSlVVRaEXuoGamYi5K5430HNiTiEWHKJg05eRWgNfKeV7RxbqUhGKPV/207VupQ8is0IoX5vtFC18SqEHaK4GyHTZ2kzVR8PBTCO4oANIZL4ShNVZcOhKKeYg9DoWdhI1ec3os2VFI0JCIUez5+i6st0qJZRrEAIJCw+QdW223BG/EmKwTBc/IJ/qfp2FDrkUnwFo8U9dZyqnaPhxLqfYjyM1S3vb6p+GGOBszsojoTDSDFz6qj66R4LzvYJxVMwUNRjf1H1ywQr/megg2RzLximy8waqvbda8M5iijegVEiHjlM1W/3h+FcXesphsMY4dMOUnUgOxyuPEzxPQwRNvV3qg5Nj4BreyimwADWe/dRVTMjEm6MoGLzGwtystL6RyOY3qSqdlYU3FpLZw1VW0sK5943MvUCKwJ1noNtjs6Ohge76Zq9ZkfpigU5WWkDYuCfbs1U5HWFR8/Qq4a9W0uK5k4ZmdrTCl8spGIePLPlbqqsc1Afe83O0hULc8alDYiBd7ZyitYMeBfR55rR2fOKP6ioPk2dGvZ+UVI0d8rtqT2tcCexlqK2F3wRn5Q+YVbBqrLKOupkr9lZujAOrmS0UpTb4JeIPkNHZ+cXr6uoPk2vyuBSPhWLEKj45PQJuQWryyqP0Z14uGLdROHIRNBEXDR09EP5r62rOHCazhrD4VKPwxTH+sIA3ZPTJ+YuWV22n+IruHFDC8X2CBjnPoolcGc2FYUwzmsUWXDHsoGKLBhmN0VvuBVfTVE/AAbpaid5CB4MbaLY1QXGuIViLTyZQcVyGGMuxWPwaA0Vk2GI9RRp8Ci2iuLkIBjhT5LNUfAspZFiTwyC72KK7+DNg1SsRvCNp3gZXq2k4iEEXSHFJHgVXUlxejCCbTvFAHiXdIJiXxyCK7KJ5FHoMZGK9xBcwyg2QpdlVMxEUM2iyIMuXXZQNF+HswxMsSAAJRQjoE//eoqDCXBSTO6f1xd+O0iyNRY6jaWi1ALNYCocZROj4JdEikroVkjFk9DcStXxpdfCD2MoXodu4RUU9ptxxmXssOfxnvDVcxRTod9FxyhqLoAqis5aPhwTDp9spRgEH2Q6KLbYoKqlaKTm6Isp0C/sJMnjFvhiERXPQvUNRe9p29lhR04CdBpC8Sl8YiuncIxEuzUUg4Dkgj+paVozygY9plPMh28SaymO9kabAopREGF3vt9MzeFFl8G7lRSZ8FFGK8XX4VA8QjEd7XrM3M0OXz8YCy+qKBLgq3wqnofiTorF0Ax56Rg1J1elW+BBAsVe+My6iYq7IK6keBdOIseV2qn5Pb8f3MqkWAXf9ThM8c8lAOIotuFsF875lRrH5klRcG0+xcPwQ1oLxfeRAP4heQTnGL78X2rqlw2DK59SXAV/zKaiGMAuko5InCt68mcOan5+ohf+z1pP8lQY/GHZQMV4YD3FpXDp4qerqbF/lBWBswyi+AL+ia+maLgcRRQj4IYlY/UpauqKBsPJAxQF8NM1TRQ/RudSPAD34rK3scOuR8/HGcspxsJfOVS8NZbiGXiUtPgINU3v3WFDmx8pEuG3EiqKKVbCC1vm2iZqap5LAtCtleQf8F9sFYWDohzeJczYyQ4V2bEZFGsQgJRGqqqhS2phHTWn9lDkIhBTqWqxQZ+IsRvtdHY9AvI2VX2hW68nfqGmuQsCEl3JdjfCF8OW1bPdtwhQ0gm2mQzfRE3a7KCYj0BNZJs8+Kxf/r6WtTEI2FIqlsMfFgRB5A6KUnSe/vUkX0AnuvUIt8SjM1m6wWQymUwmk8lkMgXRf5vi8rLQxtUhAAAAAElFTkSuQmCC";
 //Respuesta de la ruta padre
 router.get("/api/catalogos-libros/", function (req, res) {
-    mysqlConection.query("SELECT libros.* FROM libros LEFT JOIN detalleCatalogo "
-        + "ON libros.idLibro = detalleCatalogo.id_libro WHERE detalleCatalogo.id_libro IS NULL", (err, rows, fields) => {
+    mysqlConection.query("SELECT libros.* FROM libros", (err, rows, fields) => {
+
+            /* mysqlConection.query("SELECT libros.* FROM libros LEFT JOIN detalleCatalogo "
+            + "ON libros.idLibro = detalleCatalogo.id_libro WHERE detalleCatalogo.id_libro IS NULL", (err, rows, fields) => { */
             if (!err) {
                 //res.json(newArray); //Muestra el resultado
 
@@ -421,56 +423,112 @@ router.get("/api/createCatalogo/:number", function (req, res) {
                                     })
                                 )
                             }
-                            if (newArray[i].libros[j].autor) {
+
+                            //Autor
+
+                            if (newArray[i].libros[j].autor != null && newArray[i].libros[j].autor != "null" && newArray[i].libros[j].autor != "" && newArray[i].libros[j].autor != " ") {
                                 textoCont.push(new TextRun({
                                     text: newArray[i].libros[j].autor + ".",
                                     size: 22,
                                 }))
                             }
-                            if (newArray[i].libros[j].titulo) {
+                            //Título
+
+                            if (newArray[i].libros[j].titulo != null && newArray[i].libros[j].titulo != "null" && newArray[i].libros[j].titulo != " "&& newArray[i].libros[j].titulo != "") {
                                 textoCont.push(new TextRun({
                                     text: " " + newArray[i].libros[j].titulo,
                                     size: 22,
                                     bold: true,
                                 }))
                             }
-                            if (newArray[i].libros[j].editorial) {
+                            //Autor, otra vez, si se puede, si no, no hay problema
+                            if (newArray[i].libros[j].autor != null && newArray[i].libros[j].autor != "null" && newArray[i].libros[j].autor != "" && newArray[i].libros[j].autor != " ") {
+                                textoCont.push(new TextRun({
+                                    text: ", " +newArray[i].libros[j].autor,
+                                    size: 22,
+                                }))
+                            }
+                            //Coordinadores
+                            if (newArray[i].libros[j].coordinadores != null && newArray[i].libros[j].coordinadores != "null" && newArray[i].libros[j].coordinadores != "" && newArray[i].libros[j].coordinadores != " ") {
+                                textoCont.push(new TextRun({
+                                    text: ", " + newArray[i].libros[j].coordinadores ,
+                                    size: 22,
+                                }))
+                            }
+                            //Lugar de publicación
+                            if (newArray[i].libros[j].placePub != null && newArray[i].libros[j].placePub != "null" &&newArray[i].libros[j].placePub != "" &&newArray[i].libros[j].placePub != " ") {
+                                textoCont.push(new TextRun({
+                                    text: ", " + newArray[i].libros[j].placePub,
+                                    size: 22
+                                }))
+                            }
+                            //Editorial(es)
+
+                            if (newArray[i].libros[j].editorial != null && newArray[i].libros[j].editorial != "null" &&  newArray[i].libros[j].editorial != "" && newArray[i].libros[j].editorial != " ") {
                                 textoCont.push(new TextRun({
                                     text: ", " + newArray[i].libros[j].editorial,
                                     size: 22
                                 }))
                             }
 
-                            if (newArray[i].libros[j].placePub) {
+                            //Año de edición
+                            if (newArray[i].libros[j].anio != null && newArray[i].libros[j].anio != "null" && newArray[i].libros[j].anio != "" && newArray[i].libros[j].anio != " ") {
                                 textoCont.push(new TextRun({
-                                    text: ", " + newArray[i].libros[j].placePub,
+                                    text: ", " + newArray[i].libros[j].anio,
                                     size: 22
                                 }))
                             }
-                            if (newArray[i].libros[j].dimensiones) {
+                           // Número de páginas
+                           if (newArray[i].libros[j].paginas != null && newArray[i].libros[j].paginas != "null" && newArray[i].libros[j].paginas != "" && newArray[i].libros[j].paginas != " ") {
+                            textoCont.push(new TextRun({
+                                text: ", " + newArray[i].libros[j].paginas + " paginas",
+                                size: 22
+                            }))
+                        }
+                        
+                        //Notas bibliográficas
+                        if (newArray[i].libros[j].nota != null && newArray[i].libros[j].nota != "null"  && newArray[i].libros[j].nota != "") {
+                            textoCont.push(new TextRun({
+                                text: ", " + newArray[i].libros[j].nota,
+                                size: 22
+                            }))
+                        }
+                        //Dimensión, solamente los centímetros
+                            if (newArray[i].libros[j].dimensiones != null && newArray[i].libros[j].dimensiones != "null" && newArray[i].libros[j].dimensiones != "" && newArray[i].libros[j].dimensiones != " ") {
                                 textoCont.push(new TextRun({
-                                    text: ", Dimensiones " + newArray[i].libros[j].dimensiones,
+                                    text: ", " + newArray[i].libros[j].dimensiones,
+                                    size: 22
+                                }))
+                            }
+                            //Y por último, el ISBN
+                            
+                            if (newArray[i].libros[j].issn != null && newArray[i].libros[j].issn != "null" && newArray[i].libros[j].issn != "" && newArray[i].libros[j].issn != " ") {
+                                textoCont.push(new TextRun({
+                                    text: ", " + newArray[i].libros[j].issn,
                                     size: 22
                                 }))
                             }
 
-                            if (newArray[i].libros[j].paginas) {
+                            if (newArray[i].libros[j].isbn != null && newArray[i].libros[j].isbn  != "null" && newArray[i].libros[j].isbn  != "" && newArray[i].libros[j].isbn  != " " ) {
                                 textoCont.push(new TextRun({
-                                    text: ", " + newArray[i].libros[j].paginas + " paginas",
+                                    text: ", " + newArray[i].libros[j].isbn,
                                     size: 22
                                 }))
                             }
+
                             if (newArray[i].libros[j].descripcion) {
                                 textoCont.push(new TextRun({
                                     text: ", " + newArray[i].libros[j].descripcion,
                                     size: 22
                                 }))
                             }
+
                             arrayContenido.push(
                                 new Paragraph({
                                     children: textoCont
                                 })
                             )
+
                             arrayContenido.push(
                                 new Paragraph({
                                     children: [new TextRun({
@@ -481,6 +539,7 @@ router.get("/api/createCatalogo/:number", function (req, res) {
                                 })
                             )
                         }
+
                         console.log("------------------")
                     }
                 }
